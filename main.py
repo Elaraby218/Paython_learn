4bae47b1f39d19182ca78a72902bf155f57649e6
Elaraby218 <icpcnews5@gmail.com>
2024-02-16 20:18:25 +0200

    Strings , Floats , Intgers , Booleans , String casting , 15:00

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("helloklxc")
+#print("helloklxc")
+
+# strings
+
+#first_name = "mohamed"
+#last_name = "code"
+#full_name = first_name + " " + last_name
+#print(full_name)
+
+# intgers
+
+#age = 21
+#age += 1
+#print("your age is " +str(age))   # str(datatype) -- > casting to string
+#print(type(age))
+
+# Floats
+#height = 250.234456456456456456456456456456456546456456456356746758456743758
+#print("your height is " + str(height))
+#print(type(height))
+
+# Boolean data type
+#human = True
+#print(human)

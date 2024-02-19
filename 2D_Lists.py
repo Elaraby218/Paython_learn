c933e909a445bc043e75a3ccb20649eff42d80e5
Elaraby218 <icpcnews5@gmail.com>
2024-02-19 13:20:29 +0200

    Lists and some of its functions

---
+++ 2D_Lists.py
@@ -0,0 +1,12 @@
+drinks = ["Coffee" , "Soda" , "tea"]
+dinner = ["Pizza" , "hotdog" , "ice cream"]
+dessert= ["cake" , "ice cream"]
+
+food = [drinks , dinner,dessert]
+
+for i in food:
+    for j in i:
+        print(j , end=" ")
+    print("")
+
+# https://youtu.be/XKHEtdqhLK8?t=5234 
